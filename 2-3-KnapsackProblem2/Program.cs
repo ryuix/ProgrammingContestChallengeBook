dd9d1227f05ef628af648189817566613f485c68
ryuix <ryu1mizukami@gmail.com>
2022-11-01 18:09:07 +0900

    add 2-3 Knapsacks

---
+++ 2-3-KnapsackProblem2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+namespace _2_3_KnapsackProblem2
+{
+    internal class Program
+    {
+        static int n;
+        static int[] w, v;
+        static int W;
+
+        static void Main(string[] args)
+        {
+            using var sr = new StreamReader("input.txt");
+            Console.SetIn(sr);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Input();
+            Solve();
+            sw.Stop();
+            var ts = sw.Elapsed;
+
+            Console.WriteLine($"{ts.Seconds}:{ts.Milliseconds}");
+        }
+
+        private static void Solve()
+        {
+            var maxV = v.Sum();
+
+            // dp[i+1][j] i番目までの品物から価値の総和がjとなるように選んだ時の、重さの総和の最小値
+            var dp = Enumerable.Repeat(0, n + 1).Select(_ => Enumerable.Repeat(0, maxV + 1).ToArray()).ToArray();
+            for (int i = 1; i <= maxV; i++)
+            {
+                dp[0][i] = (int)(1e+9 + 1);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= maxV; j++)
+                {
+                    if (j < v[i])
+                    {
+                        dp[i + 1][j] = dp[i][j];
+                    }
+                    else
+                    {
+                        dp[i + 1][j] = Math.Min(dp[i][j], dp[i][j - v[i]] + w[i]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(string.Join(' ', dp[i]));
+            }
+
+            var result = 0;
+            for (int i = 0; i <= maxV; i++)
+            {
+                if (dp[n][i] <= W) result = i;
+            }
+
+            Console.WriteLine(result);
+
+        }
+
+        private static void Input()
+        {
+            n = int.Parse(Console.ReadLine().Trim());
+            w = new int[n];
+            v = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Trim().Split(' ');
+                (w[i], v[i]) = (int.Parse(line[0]), int.Parse(line[1]));
+            }
+
+            W = int.Parse(Console.ReadLine().Trim());
+        }
+    }
+}
---
+++ 2-3-KnapsackUnlimited/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+namespace _2_3_KnapsackUnlimited
+{
+    internal class Program
+    {
+        static int n;
+        static (int w, int v)[] items;
+        static int W;
+
+        static void Main(string[] args)
+        {
+            using var sr = new StreamReader("input.txt");
+            Console.SetIn(sr);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Input();
+            //Solve();
+            Solve2();
+            sw.Stop();
+            var ts = sw.Elapsed;
+
+            Console.WriteLine($"{ts.Seconds}:{ts.Milliseconds}");
+
+        }
+
+        // 3Loop
+        private static void Solve()
+        {
+            var dp = Enumerable.Repeat(0, n + 1).Select(_ => Enumerable.Repeat(0, W + 1).ToArray()).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= W; j++)
+                {
+                    for (int k = 0; k * items[i].w <= j; k++)
+                    {
+                        dp[i + 1][j] = Math.Max(dp[i + 1][j], dp[i][j - k * items[i].w] + k * items[i].v);
+                    }
+                }
+            }
+            Console.WriteLine(dp[n][W]);
+        }
+
+        // 2Loop
+        private static void Solve2()
+        {
+            var dp = Enumerable.Repeat(0, n + 1).Select(_ => Enumerable.Repeat(0, W + 1).ToArray()).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= W; j++)
+                {
+                    if (j < items[i].w)
+                    {
+                        dp[i + 1][j] = dp[i][j];
+                    }
+                    else
+                    {
+                        dp[i + 1][j] = Math.Max(dp[i][j], dp[i + 1][j - items[i].w] + items[i].v);
+                    }
+                }
+            }
+            Console.WriteLine(dp[n][W]);
+        }
+
+        private static void Input()
+        {
+            n = int.Parse(Console.ReadLine().Trim());
+            items = new (int w, int v)[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Trim().Split(' ');
+                items[i] = (int.Parse(line[0]), int.Parse(line[1]));
+            }
+
+            W = int.Parse(Console.ReadLine().Trim());
+        }
+    }
+}
