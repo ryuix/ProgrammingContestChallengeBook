c0705b341b653d4b9da61ca6369bbe04278bf21e
ryuix <ryu1mizukami@gmail.com>
2022-11-21 16:02:23 +0900

    add 2-4 FenceRepair(PKU 3253)

---
+++ 2-4-FenceRepair/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using CSharpStl;
+
+namespace _2_4_FenceRepair
+{
+    internal class Program
+    {
+        static int n; // 切り出すべき板の数
+        static int[] l; // 切り出した板の長さ
+
+        static void Main(string[] args)
+        {
+            using var sr = new StreamReader("input.txt");
+            Console.SetIn(sr);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Input();
+            Solve();
+            //Solve2();
+            sw.Stop();
+            var ts = sw.Elapsed;
+
+            Console.WriteLine($"{ts.Seconds}:{ts.Milliseconds}");
+        }
+
+        private static void Solve()
+        {
+            var answer = 0;
+            var que = new PriorityQueue<int>(false);
+
+            for (int i = 0; i < l.Length; i++)
+            {
+                que.Push(l[i]);
+            }
+
+            while (que.Count > 1)
+            {
+                int l1, l2;
+                l1 = que.Top;
+                que.Pop();
+                l2 = que.Top;
+                que.Pop();
+
+                answer += l1 + l2;
+                que.Push(l1 + l2);
+            }
+
+            Console.WriteLine(answer);
+        }
+
+        private static void Input()
+        {
+            n = int.Parse(Console.ReadLine().Trim());
+            l = Console.ReadLine().Trim().Split(' ').Select(x => int.Parse(x)).ToArray();
+        }
+    }
+}
--- CSharpStl/PriorityQueue.cs
+++ CSharpStl/PriorityQueue.cs
@@ -1,57 +1,92 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace CSharpStl
 {
     public class PriorityQueue<T> where T : IComparable<T>
     {
         List<T> buffer;
+        bool isDescend;
 
-        public PriorityQueue() { this.buffer = new List<T>(); }
+        public PriorityQueue(bool isDescend = true)
+        {
+            this.buffer = new List<T>();
+            this.isDescend = isDescend;
+        }
 
-        static void PushHeap(List<T> array, T elem)
+        static void PushHeap(List<T> array, T elem, bool isDescend)
         {
             int n = array.Count;
             array.Add(elem);
-
-            while (n != 0)
+            if (isDescend)
             {
-                int i = (n - 1) / 2;
-                if (array[n].CompareTo(array[i]) > 0)
+                while (n != 0)
                 {
-                    (array[n], array[i]) = (array[i], array[n]);
+                    int i = (n - 1) / 2;
+                    if (array[n].CompareTo(array[i]) > 0)
+                    {
+                        (array[n], array[i]) = (array[i], array[n]);
+                    }
+                    n = i;
                 }
-                n = i;
+            }
+            else
+            {
+                while (n != 0)
+                {
+                    int i = (n - 1) / 2;
+                    if (array[n].CompareTo(array[i]) < 0)
+                    {
+                        (array[n], array[i]) = (array[i], array[n]);
+                    }
+                    n = i;
+                }
             }
         }
 
-        static void PopHeap(List<T> array)
+        static void PopHeap(List<T> array, bool isDescend)
         {
             int n = array.Count - 1;
             array[0] = array[n];
             array.RemoveAt(n);
-
-            for (int i = 0, j; (j = 2 * i + 1) < n;)
+            if (isDescend)
             {
-                if ((j != n - 1) && (array[j].CompareTo(array[j + 1]) < 0))
-                    j++;
-                if (array[i].CompareTo(array[j]) < 0)
+                for (int i = 0, j; (j = 2 * i + 1) < n;)
                 {
-                    (array[i], array[j]) = (array[j], array[i]);
+                    if ((j != n - 1) && (array[j].CompareTo(array[j + 1]) < 0))
+                        j++;
+                    if (array[i].CompareTo(array[j]) < 0)
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                    }
+                    i = j;
                 }
-                i = j;
+            }
+            else
+            {
+                for (int i = 0, j; (j = 2 * i + 1) < n;)
+                {
+                    if ((j != n - 1) && (array[j].CompareTo(array[j + 1]) > 0))
+                        j++;
+                    if (array[i].CompareTo(array[j]) > 0)
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                    }
+                    i = j;
+                }
             }
         }
 
         public void Push(T elem)
         {
-            PushHeap(this.buffer, elem);
+            PushHeap(this.buffer, elem, isDescend);
         }
 
         public void Pop()
         {
-            PopHeap(this.buffer);
+            PopHeap(this.buffer, isDescend);
         }
 
         public T Top
